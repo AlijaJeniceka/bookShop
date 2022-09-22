91cdef2e96152fbb45ecd1d74bbbc4bfaf2de00f
alija.jeniceka <alija.juldaseva@gmail.com>
2022-09-22 12:16:40 +0300

    h2 db not runing for test

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,4 +2,4 @@
 INSERT INTO BOOK (id, title, author, genre, release_year, isbn, quantity, price)
 VALUES (1, 'Title', 'Author', 'Genre', 2022, '000-00-00000-00-0', 1, 5);
 INSERT INTO BOOK (id, title, author, genre, release_year, isbn, quantity, price)
-VALUES (2, 'Title2', 'Author2', 'Genre2', 2022, '000-00-00000-00-1', 2, 4);
+VALUES (2, 'Title2', 'Author2', 'Genre2', 2022, '000-00-00000-00-1', 2, 4;
